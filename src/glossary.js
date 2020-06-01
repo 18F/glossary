449d142eba3d49856f1f2b7d5841b1a533bbfad1
Caleb Schwind <caleb.schwind@erg.com>
2020-06-01 11:06:27 -0400

    Remove some of the test code.

--- src/glossary.js
+++ src/glossary.js
@@ -51,13 +51,11 @@ var itemTemplate = function(values) {
     '" data-glossary-term-value="' +
     values.term.toLowerCase() + 
     '">' +
-    '<button class="data-glossary-term testing ' +
+    '<button class="data-glossary-term ' +
     values.termClass +
     '">' +
     values.term +
     '</button>' +
-    '<button class="js-glossary-test">Test</button>' +
-    '<button class="js-glossary-test2">Test2</button>' +
     '<div class="' +
     values.definitionClass +
     '">' +
